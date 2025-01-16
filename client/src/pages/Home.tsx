79c6a7df15614b1181920b8ca48bc62c2a31ae72
Joshua <joxuecont@gmail.com>
2025-01-15 23:11:24 -0400

    Refactor Home component layout for improved responsiveness and styling

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,21 +4,23 @@ import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <div className='flex justify-between items-center'>
+    <div className='flex flex-col sm:flex-row sm:justify-between sm:items-center'>
       <div>
         <p className='text-lg font-medium text-gray-500 mt-5'>
           {getFullDate()}
         </p>
-        <h1 className='text-4xl font-semibold'>Welcome back, User</h1>
+        <h1 className='text-3xl sm:text-4xl font-semibold'>
+          Welcome back, User
+        </h1>
       </div>
 
-      <nav>
+      <nav className='w-full sm:w-auto'>
         <Link
           to='/projects/create'
-          className='flex items-center bg-purple-500 hover:bg-purple-600 text-white text-lg font-medium py-2 px-4 rounded-xl transition-colors'
+          className='flex items-center justify-center bg-purple-500 hover:bg-purple-600 text-white text-lg font-medium py-2 px-4 rounded-xl transition-colors mt-6 sm:mt-0'
         >
           <Plus className='inline' />
-          <p className='hidden md:block ml-2'>New Project</p>
+          <p className='ml-2'>New Project</p>
         </Link>
       </nav>
     </div>
