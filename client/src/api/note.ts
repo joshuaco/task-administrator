0fd0a6b915f0b97c1903786a9b053b9cf3c40387
Joshua <joxuecont@gmail.com>
2025-03-11 11:00:21 -0400

    Implement deleteNote functionality and integrate it into TaskStatusLog component

--- client/src/api/note.ts
+++ client/src/api/note.ts
@@ -1,11 +1,12 @@
 import api from '@/lib/axios';
-import { Project, Task, TaskNoteForm } from '@/types';
+import { Project, Task, TaskNote, TaskNoteForm } from '@/types';
 import { isAxiosError } from 'axios';
 
 interface NoteAPIProps {
   formData: TaskNoteForm;
   projectId: Project['_id'];
   taskId: Task['_id'];
+  noteId: TaskNote['_id'];
 }
 
 export async function createNote({
@@ -16,6 +17,22 @@ export async function createNote({
   try {
     const url = `/projects/${projectId}/tasks/${taskId}/notes`;
     const { data } = await api.post(url, formData);
+    return data.message;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+}
+
+export async function deleteNote({
+  projectId,
+  taskId,
+  noteId
+}: Pick<NoteAPIProps, 'projectId' | 'taskId' | 'noteId'>) {
+  try {
+    const url = `/projects/${projectId}/tasks/${taskId}/notes/${noteId}`;
+    const { data } = await api.delete(url);
     return data.message;
   } catch (error) {
     if (isAxiosError(error) && error.response) {
